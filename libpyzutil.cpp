42595373ece4bd855ea7b44028c15caddd2c62cf
David Crayford <DCrayford@rocketsoftware.com>
2024-02-20 18:54:48 +0800

    Fix garbled encoding

--- libpyzutil.cpp
+++ libpyzutil.cpp
@@ -1,1 +1,79 @@
-{‰•ƒ“¤„…@Lƒ¢£™‰•‡n{‰•ƒ“¤„…@Lƒ¢£„‰•£n{‰•ƒ“¤„…@Lƒ£‰”…n{‰•ƒ“¤„…@Lƒ¢‰‡•“n{‰•ƒ“¤„…@Lƒ¢…£‘”—n{‰•ƒ“¤„…@Lƒ¢£„“‰‚n{‰•ƒ“¤„…@L‰–¢£™…”n…§£…™•@Ã@À„–¤‚“…@—¨©¤£‰“mƒ“–ƒ’M]@À@@@@™…£¤™•@MM„–¤‚“…]@ƒ“–ƒ’M]]@a@ÃÓÖÃÒâm×ÅÙmâÅÃ^Ğ‰•£@—¨©¤£‰“m¢£ƒ’Mƒˆ™@\–¤£‚¤†k@¢‰©…m£@¢‰©…]@À@@@@‰†@M¢‰©…@L@ø]@™…£¤™•@`ñ^@@@@mm¢”M@âãÃÒ@lğ@@@@@@@@@@@@z@~”M\–¤£‚¤†]@@@@@@@@@@@@z@@@@@@@@@@@@z@@@@@@@@@@@@]^@@@@™…£¤™•@ğ^Ğ‰•£@—¨©¤£‰“m¢£ƒ’…Mƒˆ™@\–¤£‚¤†k@¢‰©…m£@¢‰©…]@À@@@@‰†@M¢‰©…@L@ñö]@™…£¤™•@`ñ^@@@@mm¢”M@âãÃÒÅ@lğ@@@@@@@@@@@@z@~”M\–¤£‚¤†]@@@@@@@@@@@@z@@@@@@@@@@@@z@@@@@@@@@@@@]^@@@@™…£¤™•@ğ^Ğ¤‰•£öôm£@—¨©¤£‰“m£‰”…m¤¢…„M]À@@@@¤‰•£öôm£@”‰ƒ¢@~@ğ^@@@@mm¢”M@Ó@@@@@ñôkñöMğkğ]@@@@@@@@@Ãåã@„„™…¢¢@@@@@@@@@@@@@@@@@@@@à•@@@@@@@@@@@Ó@@@@@ñôk÷÷òMñôkğ]@@@@@@@âÆã@„„™…¢¢@@@@@@@@@@@@@@@@@@@@à•@@@@@@@@@@@Ó@@@@@ñôkòôôMñôkğ]@@@@@@@ÓçaÅç@†–™@¢…™¥‰ƒ…@™–¤£‰•…@@@@@@à•@@@@@@@@@@@ÓÁ@@@@ğkñ@@@@@@@@@@@@@@@@â…£@£‰”‰•‡@†“‡@£–@Ã×äaÔÉÃ@@@@@à•@@@@@@@@@@@×Ã@@@@ğMñô]@@@@@@@@@@@@@@×Ã@£–@ãÉÔÅäâÅÄ@¢…™¥‰ƒ…@™–¤£‰•…@à•@@@@@@@@@@@âãÔ@@@ğkñklğ@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@z@~”M”‰ƒ¢]@@aa@–¤£—¤£@@„…†‰•‰£‰–•@@@@@@@@@@@@z@@@@@@@@@@@@@@aa@•–@‰•—¤£@@@@@@@@@@@@z@™ğk@™ñk@™ñôk@™ñõ]^@@aa@ƒ“–‚‚…™@“‰¢£@@@@™…£¤™•@”‰ƒ¢@nn@ñò^Ğ‰•£@—¨©¤£‰“m£‰”…M]À@@@@‰•£@”‰ƒ¢@~@ğ^@@@@™…£¤™•@”‰ƒ¢^Ğ¢££‰ƒ@‘”—m‚¤†@‘”—‚¤†^¢££‰ƒ@¥–‰„@¢…‡¥mˆ•„“…™M‰•£@¢‰‡]@À@@@@‰†@M¢‰‡@~~@âÉÇâÅÇå]@“–•‡‘”—M‘”—‚¤†k@ñ]^@@aa@™…£¤™•@ƒ–•£™–“@£–@¦ˆ…™…@¢…£‘¤”—M]@‰¢@ƒ““…„Ğ‚––“@—©¤£‰“m¢£–™‡…M¤‰•£öôm£@„„™…¢¢k@ƒˆ™@\–¤£‚¤†k@¢‰©…m£@“…•‡£ˆ]@À@@@@ƒ–•¢£@ƒˆ™@\—£™@~@™…‰•£…™—™…£mƒ¢£Lƒ–•¢£@ƒˆ™@\nM„„™…¢¢]^@@@@‰†@M“…•‡£ˆ@~~@ğ]@™…£¤™•@†“¢…^@@@@aa@¢…£@@¢‰‡•“@ˆ•„“…™@£–@™…ƒ–¥…™@†™–”@¢…‡”…•££‰–•@†¤“£¢K@@@@¢‰‡•“MâÉÇâÅÇåk@¢…‡¥mˆ•„“…™]^@@@@aa@â¥…@£ˆ…@¢£ƒ’@…•¥‰™–•”…•£@ƒ–•£…§£K@É†@£ˆ…@¢‰‡•“@ˆ•„“…™@‰¢@ƒ““…„@„¤…@£–@@âÉÇâÅÇå@MÖÃô]@@@@aa@£ˆ…•@ƒ–•£™–“@¦‰““@‚…@™…¢£–™…„@£–@£ˆ‰¢@—–‰•£K@@@@‰•£@™ƒ@~@¢…£‘”—M‘”—‚¤†]^@@@@‰†@M™ƒ@~~@ğ]@À@@aa@¢£@@@@@@@@”…”ƒ—¨M–¤£‚¤†k@—£™k@“…•‡£ˆ]^@aa@ƒ–—¨@£ˆ…@„£@M”¨@ğÃô@‰†@£ˆ…@„„™…¢¢@‰¢@‚„]@@@@Ğ@aa@…“¢…@“–•‡‘”—M]@¦¢@ƒ““…„@†™–”@£ˆ…@¢‰‡•“@ˆ•„“…™@@@@¢‰‡•“MâÉÇâÅÇåk@âÉÇmÄÆÓ]^@@aa@“¦¨¢@¤•™…‡‰¢£…™@£ˆ…@¢‰‡•“@ˆ•„“…™@@@@™…£¤™•@™ƒ@~~@ğ^ĞĞ@aa@…§£…™•@Ã
+#include <cstring>
+#include <cstdint>
+#include <ctime>
+#include <csignal>
+#include <csetjmp>
+#include <cstdlib>
+#include <iostream>
+
+extern "C" {
+
+static odp::mvs_module *callable_service;
+static void *save_area;
+
+double pyzutil_clock() {
+    return ((double) clock()) / CLOCKS_PER_SEC;
+}
+
+int pyzutil_stck(char *outbuf, size_t size) {
+    if (size < 8) return -1;
+    __asm(" STCK %0"
+            : "=m"(*outbuf)
+            :
+            :
+            );
+    return 0;
+}
+
+int pyzutil_stcke(char *outbuf, size_t size) {
+    if (size < 16) return -1;
+    __asm(" STCKE %0"
+            : "=m"(*outbuf)
+            :
+            :
+            );
+    return 0;
+}
+
+uint64_t pyzutil_time_used()
+{
+    uint64_t mics = 0;
+    __asm(" L     14,16(0,0)         CVT address                    \n"
+          " L     14,772(14,0)       SFT address                    \n"
+          " L     14,244(14,0)       LX/EX for service routine      \n"
+          " LA    0,1                Set timing flag to CPU/MIC     \n"
+          " PC    0(14)              PC to TIMEUSED service routine \n"
+          " STM   0,1,%0                                              "
+            : "=m"(mics)  // output  definition
+            :              // no input
+            : "r0", "r1", "r14", "r15");  // clobber list
+    return mics >> 12;
+}
+
+int pyzutil_time()
+{
+    int mics = 0;
+    return mics;
+}
+
+static jmp_buf jmpbuf;
+static void segv_handler(int sig) {
+    if (sig == SIGSEGV) longjmp(jmpbuf, 1);  // return control to where setjump() is called
+}
+
+bool pzutil_storage(uint64_t address, char *outbuf, size_t length) {
+    const char *ptr = reinterpret_cast<const char *>(address);
+    if (length == 0) return false;
+    // set a signal handler to recover from segmentation faults.
+    signal(SIGSEGV, segv_handler);
+    // Save the stack environment context. If the signal handler is called due to a SIGSEGV (OC4)
+    // then control will be restored to this point.
+    int rc = setjmp(jmpbuf);
+    if (rc == 0) {  // sta
+        memcpy(outbuf, ptr, length); // copy the data (may 0C4 if the address is bad)
+    } // else longjmp() was called from the signal handler
+    signal(SIGSEGV, SIG_DFL);  // always unregister the signal handler
+    return rc == 0;
+}
+
+} // extern "C"
